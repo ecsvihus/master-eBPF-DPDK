faf01564a06ebf8ce365880717ebcc76b0757c4b
kub2 <ecsvihus@stud.ntnu.no>
2025-02-20 10:26:58 +0000

    changed host ip addres to .2 to match host names

--- scripts/veth-setup.sh
+++ scripts/veth-setup.sh
@@ -11,9 +11,9 @@ fi
 sudo ip netns add $1
 sudo ip link add name $1-host type veth peer name $1-client
 sudo ip link set $1-client netns $1
-sudo ip netns exec $1 ip addr add 192.168.$2.1/24 dev $1-client
+sudo ip netns exec $1 ip addr add 192.168.$2.2/24 dev $1-client
 sudo ip netns exec $1 ip link set $1-client up
 sudo ip netns exec $1 ip link set lo up
 sudo ip link set $1-host up
-sudo ip route add 192.168.$2.1/32 dev $1-host
-sudo ip netns exec $1 ip route add default via 192.168.$2.1 dev $1-client
+sudo ip route add 192.168.$2.2/24 dev $1-host
+sudo ip netns exec $1 ip route add default via 192.168.$2.2 dev $1-client
